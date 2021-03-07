5d805640cdbf25b5a47310827d388dd296b8dc5a
Alexa Summers <alexas0202@gmail.com>
2021-03-07 01:56:29 -0600

    Update Time.deltaTime
    Added FixedUpdate()

--- Assets/Scripts/TimedeltaTime.cs
+++ Assets/Scripts/TimedeltaTime.cs
@@ -12,4 +12,9 @@ public class TimedeltaTime : MonoBehaviour
 
         transform.Translate (speed * Time.deltaTime, 0, 0); //creates equal movement regardless of different fps rates
     }
+
+    void FixedUpdate() //called every .02 seconds
+    {
+         Debug.Log("Time.deltaTime value (fixed update): " + Time.deltaTime + " || Frames Per Second: " +  1.0f / Time.smoothDeltaTime); //Displays Time.deltaTime and FPS values in console
+    }
 }
